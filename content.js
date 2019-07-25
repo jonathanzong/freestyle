7c10d1bce32f326ca7083f082476e1d9617e07a9
Jonathan Zong <jonathan.zong@gmail.com>
2019-07-25 01:18:49 -0400

    something

--- content.js
+++ content.js
@@ -3,16 +3,22 @@ console.log('hey');
 DomOutline({
   onClick: (element, event) => {
     console.log(element)
-    // const xpath = nodeToXPath(element);
-    const selector = UTILS.cssPath(element);
+    let selector = element.nodeName + Array.from(element.classList).map((x) => '.' + x).join('') + ':nth-child(' + ($(element).index() + 1) + ')';
+    while (element.nodeName !== 'BODY') {
+      element = element.parentNode;
+      selector = element.nodeName + ' > ' + selector;
+    }
     console.log(selector);
+    // const xpath = nodeToXPath(element);
+    // const selector = dompath(element).toCSS();
+    // console.log(selector);
     console.log($(selector));
 
-    const cleanSelector = selector.replace(/(\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*)+/g, '');
-    console.log(cleanSelector);
-    console.log($(cleanSelector));
+    // const cleanSelector = selector.replace(/(\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*)+/g, '').replace(/:nth-child\(\w+\)/g, '');
+    // console.log(cleanSelector);
+    // console.log($(cleanSelector));
 
-    const cssString = cleanSelector + "{ border: 1px solid purple; }";
+    const cssString = selector + "{ border: 1px solid purple !important; }";
     console.log(cssString);
 
     chrome.runtime.sendMessage({cssString});
